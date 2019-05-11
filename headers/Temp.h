6935f449bb7dac202635ae59b4ab447bfb70f977
SgtBacon <45608814+SgtBacon@users.noreply.github.com>
2019-05-10 19:50:04 -0700

    Added Print Function for QuickSort

--- headers/Temp.h
+++ headers/Temp.h
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 #include <array>
 using std::array;
 
 
-std::array<int, 10> MyArray = {100, 92, 20, 1, 0, 47, 47, 74, 75, 76};
+array<int, 10> MyArray = {100, 92, 20, 1, 0, 47, 47, 74, 75, 76};
 
 int FindPivot(int array[], int LowValue, int HighValue){
+    LowValue = array[0];    //Set The lower bound equal to the first element within the array
     int i = LowValue+1;
     int pivot = array[LowValue];
     for(int j = LowValue+1;j<= HighValue;j++){
@@ -16,6 +19,16 @@ int FindPivot(int array[], int LowValue, int HighValue){
     }
     std::swap(array[LowValue], array[i-1]);
     return i-1;
+
+}
+
+void PrintArray(const int (&array)[]){
+    int i=0;                    //Did this because a for loop would not work
+    i=array[0];
+    do{
+        std::cout << array[i] << std:: endl; // so instead of using a for loop to iterate through, I use a do-while loop that
+        i++;                                 //prints out the current index of the array before iterating, up until the array at index i is NULL
+    } while(array[i] != NULL);
 
 }
 void QuickSort(int array[], int LowValue, int HighValue){
